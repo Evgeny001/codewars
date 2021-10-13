c30e025579d36e2b0dde42da1edc223fbec2211e
Evgeny <info.elukas@gmail.com>
2021-10-13 22:57:26 +0300

    add new code

--- code2.js
+++ code2.js
@@ -3,8 +3,13 @@ var square = function (a) {
    return a * a;
 }
 
-function getAverage(marks){
+function getAverage(marks) {
    //TODO : calculate the downward rounded average of the marks array
    return Math.trunc(marks.reduce((sum, value) => sum + value, 0) / marks.length);
- } 
- 
+}
+
+
+function getAverage(marks) {
+   //TODO : calculate the downward rounded average of the marks array
+   return Math.trunc(marks.reduce((sum, value) => sum + value, 0) / marks.length)
+}

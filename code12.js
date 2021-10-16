c24b9634bba15eba39878d2fd89f925dc70c30e7
Evgeny <info.elukas@gmail.com>
2021-10-16 21:20:56 +0300

    16nov 5 task

---
+++ code12.js
@@ -0,0 +1,9 @@
+function isPalindrome(x) {
+   if (x.length < 2) return true;
+   for (let i = 0; i < x.length; i++) {
+     if (x[i].toLowerCase() != x[x.length - (i + 1)].toLowerCase()) {
+       return false;
+     }
+   }
+   return true;
+ }

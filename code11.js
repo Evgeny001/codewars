38e47e323988607a1e65fee9a89d0753b7160530
Evgeny <info.elukas@gmail.com>
2021-10-16 21:16:48 +0300

    16nov 3 task

--- code11.js
+++ code11.js
@@ -2,3 +2,24 @@ const areaOrPerimeter = function(l , w) {
    // Return your answer
    return l === w ? l * w : (l + w) * 2;
  };
+ 
+
+ // new code 
+ 
+function updateLight(current) {
+   
+  switch (current) {
+  case "green":
+    return "yellow";
+    break;
+  case "yellow":
+    return "red";
+    break;
+  case "red":
+    return "green"
+    break;
+  default:
+    return ""
+  }
+
+}

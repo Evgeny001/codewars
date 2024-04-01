a20dcd0bcebc9b5c199949befa7980ec37bf23c0
Diana-Balaban98 <diana.rud.2012@mail.ru>
2024-04-01 23:13:59 +0300

    commit change code13

--- code13.js
+++ code13.js
@@ -2,3 +2,8 @@ function squareArea(A){
    const s = A * 4 / (Math.PI * 1);
    return +(s * s).toFixed(1);
  }
+
+//  // function squareArea(A){
+//    const s = A * 4 / (Math.PI * 1);
+//    return +(s * s).toFixed(1);
+//  }

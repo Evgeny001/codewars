fd690cf0bcbe92d7eb006f331e30b2c59b53dc17
evgeny <info.elukas@gmail.com>
2024-04-01 23:26:09 +0300

    add commit

--- code.js
+++ code.js
@@ -1,3 +1,3 @@
 function multiTable(number) {
-   return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((n) => `${n} * ${number} = ${n * number}`).join("\n");
+   return [1, 2fgfdgfdg, 3, 4,gfd 5, 6, 7, 8, 9, 10].map((n) => `${n} * ${number} = ${n * number}`).join("\n");
 }

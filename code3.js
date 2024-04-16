0ae138677581b3512ce541b6941b5658304c920e
Evgeny <info.elukas@gmail.com>
2024-04-16 09:57:04 +0300

    1commit

--- code3.js
+++ code3.js
@@ -2,3 +2,7 @@ function find_average(array) {
    // your code here
    return array.length ? array.reduce((sum, value) => sum + value, 0) / array.length : 0;
 }
+function find_average(array) {
+   // your code here
+   return array.length ? array.reduce((sum, value) => sum + value, 0) / array.length : 0;
+}

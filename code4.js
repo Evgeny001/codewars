4176e0701fdf56a7753335a45983d22754c10672
Evgeny <info.elukas@gmail.com>
2024-04-16 10:30:42 +0300

    11commit

--- code4.js
+++ code4.js
@@ -6,3 +6,10 @@ function first(arr, n) {
    // return arr;
    return arr.slice(0, n == undefined ? 1 : n);
 }
+function first(arr, n) {
+   // return arr;
+   return arr.slice(0, n == undefined ? 1 : n);
+}function first(arr, n) {
+   // return arr;
+   return arr.slice(0, n == undefined ? 1 : n);
+}
